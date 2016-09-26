c0d0dbb97b233dfbfabb8f546c40960722453d52
Rajesh Seendripu <seendripu.rajesh@gmail.com>
2016-09-26 22:02:29 +0530

    Update config.php

--- backend/config.php
+++ backend/config.php
@@ -3,6 +3,6 @@
 
 	define("DB_HOST","mysql.topicchat.in");
 	define("DB_USER","sismol1");  //database user name
-	define("DB_PASSWORD","8978129129Bablu"); //database password
+	define("DB_PASSWORD","************"); //database password
 	define("DB_DATABASE","letslearn"); //database name
 ?>
